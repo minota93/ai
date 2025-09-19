ac6109bdc25fad30d91f81425dcc26e66bdd2750
minota93 <u01058808454@gamil.com>
2025-09-19 18:01:54 +0900

    튜플, 딕셔너리, 셋

--- source/01_python/lst.py
+++ source/01_python/lst.py
@@ -1,6 +1,11 @@
-def safe_index(lst,item):
-    """첫번째 매개변수 lst에서 item 요소가 있는 index를 반환. 
-    itme 요소가 없으면 -1 반환"""
-    if item in lst:
-        return lst.index(item)
-    return -1
+def safe_index(lst,item,start=0):
+    """
+    첫번째 매개변수 lst : 리스트, 튜플
+    두번쩨 매게변수 item :리스트나 튜플에서 찾을 데이터 
+    세번째 매개변수 satrt : 몇 번째 ondex부터 찾을지 index tn(기본값=0) 
+    itme 요소가 없으면 -1 반환
+    """
+    return lst.index(item,start) if item in lst[start:] else -1
+#     if item in lst[start:]:
+#         return lst.index(item,start) #sater번쨰 부터 item이 있는 index를 반환
+#     return -1
